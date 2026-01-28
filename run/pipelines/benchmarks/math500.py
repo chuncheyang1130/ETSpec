cf6d68027854a7d2e76ea9316ebcf423c2d7d86d
chuncheyang1130 <ccy.angus1130@gmail.com>
2026-01-28 22:04:15 +0800

    refactor: :zap: modify math-500 prompt add parser handler for math-500

--- run/pipelines/benchmarks/math500.py
+++ run/pipelines/benchmarks/math500.py
@@ -1,16 +1,37 @@
 # Prompt template adapted from https://github.com/openai/simple-evals/tree/main
 from datasets import load_dataset
 
-QUERY_TEMPLATE = """
-Solve the following math problem step by step. The last line of your response should be of the form Answer: $ANSWER (without quotes) where $ANSWER is the answer to the problem.
+REASONING_QUERY_TEMPLATE = r"""
+Given the following problem, reason and give a final answer to the problem.
+Question: {Question}
+Please reason step by step, and put your final answer within \boxed{{}}.
+""".strip()
 
-{Question}
-
-Remember to put your answer on its own line after "Answer:", and you do not need to use a \\boxed command.
+GENERAL_QUERY_TEMPLATE = r"""
+Given the following problem, reason and give a final answer to the problem.
+Problem: {Question}
+Your response should end with \"The final answer is [answer]\" where [answer] is the response to the problem.
 """.strip()
 
 # MATH-500
 def load_math500_dataset():
     dataset = load_dataset("HuggingFaceH4/MATH-500")
-    formatted_dataset = [QUERY_TEMPLATE.format(Question=entry['problem']) for entry in dataset['test']]
+    formatted_dataset = [REASONING_QUERY_TEMPLATE.format(Question=entry['problem']) for entry in dataset['test']]
     return formatted_dataset
+
+def load_math500_dataset_answer(query_version: str = "reasoning"):
+    raw = load_dataset("HuggingFaceH4/MATH-500")['test']
+    examples = []
+    for entry in raw:
+        if query_version == "reasoning":
+            q_str = REASONING_QUERY_TEMPLATE.format(Question=entry['problem'])
+        else:
+            q_str = GENERAL_QUERY_TEMPLATE.format(Question=entry['problem'])
+        a_str = entry['answer']
+        sol_str = entry['solution']
+        examples.append({
+            "question": q_str,
+            "solution": sol_str,
+            "answer": a_str
+        })
+    return examples
